4e8d7851ac6bc53f21d63d78da1a70e0fb67763d
Cristina Plesa <cristinaplesa8@gmail.com>
2021-05-03 14:50:00 -0700

    add namespace and following functionality for list of riddles
    Co-authored-by: Jonathan Stull <jonathan.d.stull@gmail.com>
    Co-authored-by: Giancarlo Vigneri <bobloblaw.vigneri@gmail.com>

---
+++ Program.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using Sphinx.Models;
+
+namespace Bridge
+{
+  public class Program
+  {
+    public static void Main()
+    {
+      Riddle one = new Riddle("What is your favorite color", "blue");
+      Riddle two = new Riddle("I’m tall when I’m young, and I’m short when I’m old. What am I?","a candle");
+      Riddle three = new Riddle("What month of the year has 28 days?","all of them");
+      Riddle four = new Riddle("What is always in front of you but can’t be seen?","the future");
+
+
+      Console.WriteLine(one.Question);
+      string guess = Console.ReadLine();
+      if (guess != one.Answer)
+      {
+        Console.WriteLine("You are wrong and so I must eat you.");
+      }
+      else if (guess == one.Answer)
+      {
+        Console.WriteLine("Your general knowledge has vanquished me!");
+      }
+    }
+  }
+}
+
+//Riddle num = new Riddle("","");
